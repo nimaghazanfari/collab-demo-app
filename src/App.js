59af668bd2e37bfc89cef3f2654a262ac665271d
NGH <ghazanfari_nima@yahoo.com>
2021-05-16 00:19:01 +0430

    Task Edit created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React, { useState } from "react";
 import Form from "./components/Form";
 import { nanoid } from "nanoid";
+import TaskItem from "./components/TaskItem";
 
 const App = ({ tasks }) => {
-  const [task, setTasks] = useState(tasks);
+  const [totalTasks, setTasks] = useState(tasks);
 
-  function addTask(name) {
+  const addTask = (name) => {
     const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
-    setTasks([...task, newTask]);
+    setTasks([...totalTasks, newTask]);
+  }
+
+  const toggleComplete = (id) => {
+
+    const idx = totalTasks.findIndex(task => task.id === id);
+    if (idx > -1) totalTasks[idx].completed = !totalTasks[idx].completed;
+
+    setTasks([...totalTasks]);
+
+  }
+
+  const removeHandler = (id) => {
+    setTasks(totalTasks.filter(item => item.id !== id));
   }
 
   return (
     <div className="stack-large todoapp" >
+
       <Form addTask={addTask} />
+
+      <div>
+        {totalTasks.length ?
+          <div>
+            {totalTasks.map((task, i) =>
+              <TaskItem key={i} item={task}
+                toggleTask={toggleComplete}
+                removeTask={removeHandler} />
+            )}
+          </div> : 'Add some items...'}
+      </div>
+
     </div>
   )
 }
---
+++ src/components/TaskEdit.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import styles from './TaskEdit.module.css';
+
+const TaskEdit = ({ item, toggleEdit, editDone }) => {
+
+    const [val, setVal] = useState(item.name);
+
+    const updateVal = (e) => {
+        setVal(e.target.value);
+    }
+
+    const editItem = () => {
+        item.name = val;
+        editDone(item);
+    }
+
+    return (
+        <span className={styles.editContainer}>
+            <form onSubmit={editItem}>
+                <input
+                    type="text"
+                    value={val}
+                    onChange={updateVal}
+                    className={styles.input}
+                />
+                <button
+                    type="submit"
+                    onClick={editItem}
+                    title="Save"
+                >
+                    &#9989;
+                </button>
+                <button
+                    type="button"
+                    onClick={toggleEdit}
+                    title="Cancel"
+                >
+                    &#10005;
+                </button>
+            </form>
+        </span>
+    )
+}
+
+export default TaskEdit;
